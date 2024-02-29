f48b3bc6f57e5b6085fe17f957d4349b47210513
rachel ibihwiori <90010676+racibih@users.noreply.github.com>
2024-02-29 13:12:00 -0500

    created sidebar effect

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "400px";
+    document.getElementById("main").style.marginLeft = "400px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    
+    document.getElementById("main").style.marginLeft = "0";
+
+}
