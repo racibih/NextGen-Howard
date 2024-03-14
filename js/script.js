2f009b3d050ad7b291ac65f2c3b2e5a7d0101c5c
rachel ibihwiori <90010676+racibih@users.noreply.github.com>
2024-03-13 21:32:41 -0400

    added bottom panel functionality

--- js/script.js
+++ js/script.js
@@ -1,13 +1,36 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "400px";
-    document.getElementById("main").style.marginLeft = "400px";
-  }
-  
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    
-    document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("bottomBar").style.width = "100%";
+  document.getElementById("main").style.marginBottom = "400px";
+  document.getElementById("bottomBar").style.display = " inline-flex";
+  document.getElementById("songBar").style.display = "none";
+  document.getElementById("favBar").style.display = "none";
 
+}
+function openSongNav() {
+  document.getElementById("songBar").style.width = "100%";
+  document.getElementById("main").style.marginBottom = "400px";
+  document.getElementById("songBar").style.display = " inline-flex";
+  document.getElementById("bottomBar").style.display = "none";
+  document.getElementById("favBar").style.display = "none";
+}
+function openFavNav() {
+  document.getElementById("favBar").style.width = "100%";
+  document.getElementById("main").style.marginBottom = "400px";
+  document.getElementById("favBar").style.display = " inline-flex";
+  document.getElementById("songBar").style.display = "none";
+  document.getElementById("bottomBar").style.display = "none";
+}
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("bottomBar").style.display = "none";
+  
+}
+function closeSongNav() {
+  document.getElementById("songBar").style.display = "none";
+  
+}
+function closeFavNav() {
+  document.getElementById("favBar").style.display = "none";
+  
 }
